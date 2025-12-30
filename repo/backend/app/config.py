a9b86024d8f9a58a5db123163d7ff8ffd7622c10
darkcomet0731 <fzh010731@icloud.com>
2025-12-30 17:29:58 +0800

    更新config.py文件

--- repo/backend/app/config.py
+++ repo/backend/app/config.py
@@ -5,6 +5,6 @@ import os
 DB_HOST = os.getenv("ZHYL_DB_HOST", "localhost")
 DB_PORT = int(os.getenv("ZHYL_DB_PORT", "3306"))
 DB_USER = os.getenv("ZHYL_DB_USER", "root")
-DB_PASSWORD = os.getenv("ZHYL_DB_PASSWORD", "394448")   #密码
+DB_PASSWORD = os.getenv("ZHYL_DB_PASSWORD", "你的密码")   #密码
 DB_NAME = os.getenv("ZHYL_DB_NAME", "zhyl")
 DB_CHARSET = os.getenv("ZHYL_DB_CHARSET", "utf8mb4")
